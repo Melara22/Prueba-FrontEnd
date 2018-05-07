4c4cbfea17e75b486437d33ceb75f50b4368f535
Melara22 <melara2396@gmail.com>
2018-05-06 23:23:16 -0600

    vali madres

---
+++ assets/js/gallery.js
@@ -0,0 +1,5 @@
+var gallery = document.querySelector('.gallery-photos');
+var msnry = new Masonry(gallery, {
+	itemSelector: '.item',
+	
+})
--- index.php
+++ index.php
@@ -21,6 +21,7 @@
   </head>
 
   <body>
+
     <nav class="navbar navbar-expand navbar-dark fixed-top">
       <a class="navbar-brand" href="#">Front end</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
@@ -46,9 +47,7 @@
         <div class="overlay"></div>
           <div class="container">
             <div class="row">
-              <div class="float-left">
-                <p>hola</p>
-              </div>
+              
               <div id="intro-text" class="col-md">
                   <h5>INTRODUCTION</h5>
                   <H1>Si se puede imaginar, se puede crear.</H1>
@@ -75,7 +74,7 @@
       </section>
       <section class="about-us" data-bg-text="About Us">
         <div class="overlay-text">
-          <h1 style="color: black;">About Us</h1>
+          <h1>About Us</h1>
         </div>
           <div class="container">
             <div class="row">
@@ -85,7 +84,7 @@
                   <img src="assets/img/about.jpg" class="img-fluid img-about mx-auto d-block">
                   
                   <div class="col-md" id="about-text">
-                      <h1>Hello.</h1>
+                      <h1 class="heading-2"><span>Hello.</span></h1>
                       <br>
                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -101,9 +100,307 @@
               
             </div>
             
+            
           </div>
         
       </section>
+
+      <section class="works">
+          <div class="row">
+              <div class="col-md  d-flex justify-content-end">
+                <div class="works-text">
+                  <h1 class="head-section">My Works</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+
+               <div class="col-md">
+                 
+               </div>              
+            </div>
+            <div class="row gallery-photos">
+                
+                  <div class="item col-md-3">
+                    <div class="overlay-info">
+                      <div class="row info">
+                        <h5>Item</h5>
+                        <p>lorem ipsum</p>                
+                      </div>
+                    </div>
+                    <img src="https://preview.ibb.co/i0PmHk/1.jpg" alt="">
+                  </div>
+                  <div class="item col-md-3">
+                    <div class="overlay-info">
+                      <div class="row info">
+                        <h5>Item</h5>
+                        <p>lorem ipsum</p>                
+                      </div>
+                    </div>
+                    <img src="https://preview.ibb.co/mWpE3Q/2.jpg" alt="">
+    
+                  </div>
+                  <div class="item col-md-3">
+                    <div class="overlay-info">
+                      <div class="row info">
+                        <h5>Item</h5>
+                        <p>lorem ipsum</p>                
+                      </div>
+                    </div>
+                    <img src="https://preview.ibb.co/i0PmHk/1.jpg" alt="">
+                  </div>
+                  <div class="item col-md-3">
+                    <div class="overlay-info">
+                      <div class="row info">
+                        <h5>Item</h5>
+                        <p>lorem ipsum</p>                
+                      </div>
+                    </div>
+                    <img src="https://preview.ibb.co/mWpE3Q/2.jpg" alt="">
+    
+                  </div>
+                  <div class="item col-md-3">
+                    <div class="overlay-info">
+                      <div class="row info">
+                        <h5>Item</h5>
+                        <p>lorem ipsum</p>                
+                      </div>
+                    </div>
+                    <img src="https://preview.ibb.co/i0PmHk/1.jpg" alt="">
+                  </div>
+                  <div class="item col-md-3">
+                    <div class="overlay-info">
+                      <div class="row info">
+                        <h5>Item</h5>
+                        <p>lorem ipsum</p>                
+                      </div>
+                    </div>
+                    <img src="https://preview.ibb.co/mWpE3Q/2.jpg" alt="">
+    
+                  </div>
+                  <div class="item col-md-3">
+                    <div class="overlay-info">
+                      <div class="row info">
+                        <h5>Item</h5>
+                        <p>lorem ipsum</p>                
+                      </div>
+                    </div>
+                    <img src="https://preview.ibb.co/i0PmHk/1.jpg" alt="">
+                  </div>
+                       
+                  
+              
+            </div>
+
+            
+         
+        
+      </section>
+
+      <section class="testimonials">
+        <div class="container">
+             <div class="row">
+              <div class="col-md">
+                <h1 class="heading-1"><span>1270</span>
+                <p>Clients</p>
+                </h1>
+
+              </div>
+              <div class="col-md">
+                <h1 class="heading-1"><span>4357</span>
+                <p>Photos</p>
+                </h1>
+              </div>
+              <div class="col-md">
+                <h1 class="heading-1"><span>634</span>
+                  <p>Sites</p>
+                </h1>
+              </div>
+              
+            </div>
+            <div class="row">
+              <h1 class="head-section">Testimonials</h1>
+              
+            </div>
+
+        </div>
+      </section>
+
+      <section class="pricing">
+         <div class="row">
+              <div class="col-md  d-flex justify-content-end">
+                <div class="works-text">
+                  <h1 class="head-section">Pricing</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+
+               <div class="col-md">
+                 
+               </div>              
+            </div>
+        <div class="row">
+          <div class="col-md product-one">
+            <div class="overlay-pricing"></div>
+              <h2>Item 1</h2>
+              <br>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <div class="col-md-6">
+                <h1>$39</h1>
+              </div>
+               <div class="col-md-6">
+                
+              </div>
+          </div>
+          <div class="col-md product-two">
+            <div class="overlay-pricing"></div>
+             <h2>Item 1</h2>
+              <br>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <div class="col-md-6">
+                <h1>$39</h1>
+              </div>
+               <div class="col-md-6">
+                
+              </div>
+          </div>
+          <div class="col-md product-three">
+            <div class="overlay-pricing"></div>
+             <h2>Item 1</h2>
+              <br>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <div class="col-md-6">
+                <h1>$39</h1>
+              </div>
+               <div class="col-md-6">
+                
+              </div>
+          </div>
+          <div class="col-md product-four"> 
+            <div class="overlay-pricing"></div>
+             <h2>Item 1</h2>
+              <br>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <h5>Photos</h5>
+              <p>lorem</p>
+              <div class="col-md-6">
+                <h1>$39</h1>
+              </div>
+               <div class="col-md-6">
+                <a class="pricing-link" href="">Explore</a>
+              </div>
+          </div>
+          
+        </div>
+        
+      </section>
+      <section class="partness">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3 col-6">
+              <img class="img-fluid" src="assets/img/logo1.png">
+            </div>
+            <div class="col-md-3 col-6">
+              <img class="img-fluid" src="assets/img/logo2.png">
+            </div>
+            <div class="col-md-3 col-6">
+              <img class="img-fluid" src="assets/img/logo3.png">
+            </div>
+            <div class="col-md-3 col-6">
+              <img class="img-fluid" src="assets/img/logo4.jpg">
+            </div> 
+            <div class="col-md-3 col-6">
+              <img class="img-fluid" src="assets/img/logo1.png">
+            </div>
+            <div class="col-md-3 col-6">
+              <img class="img-fluid" src="assets/img/logo2.png">
+            </div>
+            <div class="col-md-3 col-6">
+              <img class="img-fluid" src="assets/img/logo3.png">
+            </div>
+            <div class="col-md-3 col-6">
+              <img class="img-fluid" src="assets/img/logo4.jpg">
+            </div> 
+          </div>
+          
+        </div>
+        
+      </section>
+
+      <section class="sign-up">
+        <div class="container" align="center">
+          <h1 class="head-section">Sign up for our newsletter
+         to recieve offert</h1><br>
+         <div class="vertical-separator"> </div>
+         <br>
+          <div class="row ">
+              
+           
+              <div class="col-md form-sign" >
+                    <div class="input-group">
+                <input type="email" class="form-control" placeholder="Enter your email">
+                <span class="input-group-btn">
+                  <button class="btn btn-send" type="button">OK</button>
+                </span>
+              </div>
+            </div>
+                
+              </div>
+            
+          </div>
+          
+        
+         
+        
+      </section>
+     
+     <footer class="footer">
+      <div class="container">
+        <div class="row">
+            <div class="col-md">
+              <h5>Office</h5>
+              <p>Lorem ipsum dolor sit amet</p>
+            </div>
+            <div class="col-md">
+              <h5>Get it touch</h5>
+              <p>melara2396@gmail.com</p>
+
+            </div>
+            <div class="col-md">
+              <h5>Social</h5>
+              <p>Conec with my email and facebook</p>
+            </div>
+          
+        </div>
+      </div>
+    </footer>
 
 
 
@@ -114,6 +411,8 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/menu-effect.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.1/masonry.pkgd.min.js"></script>
+    <script src="assets/js/gallery.js"></script>
   
     
   </body>
